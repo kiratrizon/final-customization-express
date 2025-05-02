79c783498f05df86927b58d27f0890b6cdd921b8
kyuuukira <genesistroy.fdc@gmail.com>
2025-05-02 16:29:11 +0800

    Admins

--- Routes/web.js
+++ Routes/web.js
@@ -1,4 +1,5 @@
-const UserController = require('../app/Controllers/UserController');
 const Route = require('../main/express/server/Router/Route');
+
+Route.view('/', 'index');
 
 module.exports = Route;
