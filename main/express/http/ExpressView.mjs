813a086873f6ef97b99aaf2ee85e0f2c15c760f6
fdc-genesis <genesistroy.fdc@gmail.com>
2025-05-03 23:11:50 +0800

    test

--- main/express/http/ExpressView.mjs
+++ main/express/http/ExpressView.mjs
@@ -35,7 +35,9 @@ class ExpressView {
             ...this.#data
         };
 
-        let templatePath = view_path(`${viewName.split('.').join('/')}.${ExpressView.#engine}`);
+        let templatePath = path.resolve(view_path(`${viewName.split('.').join('/')}.${ExpressView.#engine}`));
+        // let dynamic = path.join(__dirname, '..', '..', '..', 'resources', 'views', `${viewName.split('.').join('/')}.${ExpressView.#engine}`);
+        // return templatePath === dynamic ? 'true' : 'false'
         if (!fs.existsSync(templatePath)) {
             return `View file not found: ${templatePath}`;
         }
