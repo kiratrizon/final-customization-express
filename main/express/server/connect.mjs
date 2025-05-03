ca23abfff83617b155ada5fb508e0e82f4e9e65e
fdc-genesis <genesistroy.fdc@gmail.com>
2025-05-03 17:47:21 +0800

    test

--- main/express/server/connect.mjs
+++ main/express/server/connect.mjs
@@ -2,6 +2,9 @@ import Server from './index.mjs';
 
 // boot
 
-await Server.boot();
+// await Server.boot();
 
+Server.app.get('/', (req, res) => {
+    res.json({ message: 'Hello World' });
+})
 export default Server.app;
