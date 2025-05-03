d79cdeea3d416f111b9377a3823e8c1f8be8509d
fdc-genesis <genesistroy.fdc@gmail.com>
2025-05-03 17:53:54 +0800

    vercel

--- index.mjs
+++
@@ -1,5 +0,0 @@
-import app from './main/express/server/connect.mjs';
-import serverless from 'serverless-http';
-
-// Wrap your app for serverless deployment
-export default serverless(app);
--- main/express/server/connect.mjs
+++ main/express/server/connect.mjs
@@ -2,9 +2,6 @@ import Server from './index.mjs';
 
 // boot
 
-// await Server.boot();
+await Server.boot();
 
-Server.app.get('/', (req, res) => {
-    res.json({ message: 'Hello World' });
-})
 export default Server.app;
