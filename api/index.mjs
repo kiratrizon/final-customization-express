7a77b38b226fc46092acd660951ac61e3e458673
fdc-genesis <genesistroy.fdc@gmail.com>
2025-05-03 18:09:53 +0800

    Test

--- api/index.mjs
+++
@@ -1,7 +0,0 @@
-import app from '../main/express/server/connect.mjs';
-import serverless from 'serverless-http';
-// Wrap your app for serverless deployment
-export default serverless(app, {
-    request: (req, res) => req,
-    response: (req, res) => res
-});
