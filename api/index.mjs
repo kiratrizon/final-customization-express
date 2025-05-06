64e753ec3f0e00b2b92577d2c6a61d41f87e5835
kyuuukira <genesistroy.fdc@gmail.com>
2025-05-06 18:03:03 +0800

    serverless

--- api/index.mjs
+++ api/index.mjs
@@ -1,4 +1,4 @@
 import app from '../main/express/server/connect.mjs';
 import serverless from 'serverless-http';
 // Wrap your app for serverless deployment
-export default app;
+export default serverless(app);
