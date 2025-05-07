8c171cacb0caf9a2ddde91467e62dd02b4784d42
kyuuukira <genesistroy.fdc@gmail.com>
2025-05-07 17:24:33 +0800

    jwt_blacklists

---
+++ main/database/migrations/1746608199379_create_jwt_blacklists_table.mjs
@@ -0,0 +1,24 @@
+import Blueprint from '../Blueprint.mjs';
+
+
+class Migrate {
+    tableName = 'jwt_blacklists';
+    up() {
+        const blueprint = new Blueprint();
+
+        const createTableSQL = blueprint.create(this.tableName, (table) => {
+            table.id();
+            table.string('token');
+            table.string('origin');
+            table.timestamp();
+        });
+
+        return createTableSQL;
+    }
+
+    down() {
+        return `DROP TABLE IF EXISTS ${this.tableName};`;
+    }
+}
+
+export default Migrate;
